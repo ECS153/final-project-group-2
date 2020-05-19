a1473b16c983143d5155a6453b958639b495e9cd
ayang282 <anthonyyang87@gmail.com>
2020-05-18 17:28:57 -0700

    Edited API

--- src/node/node_structure.js
+++ src/node/node_structure.js
@@ -1,10 +1,10 @@
 class node {
 	//constructor method
 	constructor(nodeID) {
-		this.nodeID = nodeID; 
-		this.recievedData = [];  
-		this.privateKey; 
-		this.queue = []; 
+		this.nodeID = nodeID; //to identify each node
+		this.recievedData = [];  //a queue of nodes that
+		this.privateKey; //each 
+		this.queue = []; //used to store packets ready to forward to next node
 	}
 
 	//declaring a queue storing to be forward data
@@ -34,7 +34,15 @@ class node {
 
 	//noise generator
 	//start generating noise once node object is declared
+	//send out packets with a random interval
 	noiseGenerator(){
+		return; 
+	}
+
+	//filtering out real messages from noise 
+	//the filter function should only be called at last node
+	//before posts are published to the interface
+	filter(){
 		return; 
 	}
 
